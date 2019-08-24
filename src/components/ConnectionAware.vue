69d6396a4aabe0d44d5722f100bd8f268d2ec6d8
Sacha Zvetelman <sachazvetelman@hotmail.com>
2019-08-24 18:52:02 +0100

    Some basic test cases.

--- src/components/ConnectionAware.vue
+++ src/components/ConnectionAware.vue
@@ -1,46 +1,54 @@
 <template>
   <div v-if="shouldRender">
     <slot></slot>
-    {{ minEffectiveType }}
-  </div>   
+  </div>
 </template>
 
 <script>
-const effectiveTypes = ['slow-2g', '2g', '3g', '4g'];
+const effectiveTypes = ["slow-2g", "2g", "3g", "4g"];
 
 export default {
-  name: 'ConnectionAware',
+  name: "ConnectionAware",
   props: {
     minEffectiveType: {
       type: String,
-      validator: function (value) {
+      validator: function(value) {
         return effectiveTypes.indexOf(value) !== -1;
       }
     },
     maxEffectiveType: {
       type: String,
-      validator: function (value) {
+      validator: function(value) {
         return effectiveTypes.indexOf(value) !== -1;
       }
-    } 
+    }
   },
   computed: {
-    shouldRender: function () {
-      if(!navigator) {
+    shouldRender: function() {
+      if (!navigator) {
         return true;
       }
-      
+
       const { effectiveType } = navigator.connection;
       const effectiveTypeIndex = effectiveTypes.indexOf(effectiveType);
-      const minEffectiveTypeIndex =  effectiveTypes.indexOf(this.minEffectiveType);
-      const maxEffectiveTypeIndex =  effectiveTypes.indexOf(this.maxEffectiveType);
 
-      if(effectiveTypeIndex >= minEffectiveTypeIndex || effectiveTypeIndex <= maxEffectiveTypeIndex) {
+      const minEffectiveTypeIndex = effectiveTypes.indexOf(
+        this.minEffectiveType
+      );
+
+      const maxEffectiveTypeIndex = effectiveTypes.indexOf(
+        this.maxEffectiveType
+      );
+
+      if (
+        effectiveTypeIndex >= minEffectiveTypeIndex ||
+        effectiveTypeIndex <= maxEffectiveTypeIndex
+      ) {
         return true;
       }
 
       return false;
     }
   }
-}
+};
 </script>
--- tests/unit/ConnectionAware.spec.js
+++ tests/unit/ConnectionAware.spec.js
@@ -1,16 +1,73 @@
-import { shallowMount } from '@vue/test-utils'
-import ConnectionAware from '@/components/ConnectionAware.vue'
+/* eslint-disable no-unused-labels */
+import { shallowMount } from "@vue/test-utils";
+import ConnectionAware from "@/components/ConnectionAware.vue";
 
-describe('ConnectionAware.vue', () => {
-  it('renders props.minEffectiveType when passed', () => {
-    const minEffectiveType = '3g'      
-    
-    global.navigator.connection = jest.fn();
-    
+describe("ConnectionAware.vue", () => {
+  it("renders elmenent when minEffectiveType is equal to effectiveType", () => {
+    // Arrange
+    const minEffectiveType = "3g";
+    global.navigator.connection = jest.fn(() => {
+      effectiveType: "3g";
+    });
+
+    // Act
     const wrapper = shallowMount(ConnectionAware, {
-        propsData: { minEffectiveType }
-    })
+      propsData: { minEffectiveType }
+    });
 
-    expect(wrapper.text()).toMatch(minEffectiveType);
-  })
-})
+    // Assert
+    expect(wrapper.find("div").exists()).toBe(true);
+  });
+
+  it("renders elmenent when maxEffectiveType is equal to effectiveType", () => {
+    // Arrange
+    const maxEffectiveType = "3g";
+    global.navigator.connection = jest.fn(() => {
+      effectiveType: "3g";
+    });
+
+    // Act
+    const wrapper = shallowMount(ConnectionAware, {
+      propsData: { maxEffectiveType }
+    });
+
+    // Assert
+    expect(wrapper.find("div").exists()).toBe(true);
+  });
+
+  it.each([["4g", "3g"], ["3g", "2g"], ["4g", "slow-2g"]])(
+    "renders elmenent when minEffectiveType (%s) is faster than effectiveType (%s)",
+    (minEffectiveType, effectiveType) => {
+      // Arrange
+      global.navigator.connection = jest.fn(() => {
+        effectiveType;
+      });
+
+      // Act
+      const wrapper = shallowMount(ConnectionAware, {
+        propsData: { minEffectiveType }
+      });
+
+      // Assert
+      expect(wrapper.find("div").exists()).toBe(true);
+    }
+  );
+
+  it.each([["3g", "4g"], ["2g", "3g"], ["slow-2g", "4g"]])(
+    "renders elmenent when maxEffectiveType (%s) is slower than effectiveType (%s)",
+    (maxEffectiveType, effectiveType) => {
+      // Arrange
+      global.navigator.connection = jest.fn(() => {
+        effectiveType;
+      });
+
+      // Act
+      const wrapper = shallowMount(ConnectionAware, {
+        propsData: { maxEffectiveType }
+      });
+
+      // Assert
+      expect(wrapper.find("div").exists()).toBe(true);
+    }
+  );
+});
