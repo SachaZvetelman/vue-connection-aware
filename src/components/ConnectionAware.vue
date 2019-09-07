a60c34be057b2436713fff8829e4720c45d7cb69
Sacha Zvetelman <sachazvetelman@hotmail.com>
2019-09-07 18:04:25 +0100

    Removed debugger command.

--- src/components/ConnectionAware.vue
+++ src/components/ConnectionAware.vue
@@ -52,7 +52,6 @@ export default {
   },
   computed: {
     shouldRender: function() {
-      debugger;
       if (this.connection.effectiveType == null && this.connection.isOnline == null) {
         return true;
       }
